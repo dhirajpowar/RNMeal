04f5810d7de94a10b4a26ac3022d9b5f2220749d
Dhiraj Anand Powar <dhiraj@sorigin.com>
2024-07-30 13:45:15 +0530

    Nested Navigation

--- App.js
+++ App.js
@@ -4,10 +4,29 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import { StatusBar } from 'expo-status-bar';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoriteScreen from './screens/FavoriteScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
 //351401 3f2f25
 export default function App() {
+
+  function DrawerComponent() {
+    return (
+      <Drawer.Navigator 
+        screenOptions={{
+          headerStyle: { backgroundColor: '#351401'},
+          headerTintColor: 'white',
+          sceneContainerStyle: { backgroundColor: '#3f2f25'}
+        }}
+        >
+        <Drawer.Screen name="Categories" component={CategoriesScreen} />
+        <Drawer.Screen name="Favorite" component={FavoriteScreen} />
+      </Drawer.Navigator>
+    )
+  }
   return (
     <>
       <StatusBar style='light'/>
@@ -19,10 +38,11 @@ export default function App() {
             contentStyle: { backgroundColor: '#3f2f25' }
           }}
         >
-          <Stack.Screen name="Meal Categories" component={CategoriesScreen}
+          <Stack.Screen name="Meal Categories" component={DrawerComponent}
             options={{
-              title: 'All Categories'
-            }} />
+              headerShown: false
+            }}
+            />
           <Stack.Screen name="Meal Overview"
             component={MealsOverviewScreen}
           // options={({route, navigation}) => {
@@ -33,7 +53,10 @@ export default function App() {
           />
           <Stack.Screen
             name="MealDetail"
-            component={MealDetailScreen} />
+            component={MealDetailScreen} 
+            options={{
+              title: 'About the Meal'
+            }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,11 @@
+import { View, Text} from 'react-native';
+
+function FavoriteScreen() {
+    return (
+        <View>
+            <Text>This if favorite screen. Did you like it?</Text>
+        </View>
+    )
+}
+
+export default FavoriteScreen;
